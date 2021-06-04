5d87d1d8d47f0a2198503cf1480523791b98d40d
Isaque-Pinheiro <isaquepinheiro@alu.ufc.br>
2021-06-04 17:34:26 -0300

    Semana 3

---
+++ makeParenthesesValid.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+        int minAddToMakeValid(string S) {
+            
+            int aux = 0, aux1 = 0;
+            
+            for(int i = 0; i < S.size(); i++){
+                
+                if(S[i] == '('){
+                   aux++;
+                }
+                
+                if(S[i] == ')'){
+                   if(aux > 0){
+                      aux--;
+                    }
+                
+                    else{
+                      aux1++;
+                    }
+                }
+            }
+        
+        return aux + aux1;
+       }
+};
+
