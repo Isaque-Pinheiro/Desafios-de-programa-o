d2948da5f0b6ffa35b60a39a4829e9547d670a58
Isaque-Pinheiro <isaquepinheiro@alu.ufc.br>
2021-06-04 17:33:24 -0300

    Semana 3

--- makeParentheses.cpp
+++ makeParentheses.cpp
@@ -4,7 +4,7 @@ public:
             
             int aux = 0, aux1 = 0;
             
-            for(int i=0;i<S.size();i++){
+            for(int i = 0; i < S.size(); i++){
                 
                 if(S[i] == '('){
                    aux++;
